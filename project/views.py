1c7832b9846ea3296e567377f4e02f0bca411073
ni8mr <nilpaddo@gmail.com>
2015-07-04 19:26:54 +0600

    Recreating

---
+++ project/views.py
@@ -0,0 +1,70 @@
+# project/views.py
+
+##################
+#### import ######
+##################
+
+
+from project import app, db
+from flask import flash, redirect, session, url_for, render_template, request
+from functools import wraps
+import datetime
+
+
+######################
+## helper functions ##
+######################
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (getattr(form, field).label.text, error), 'error')
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('users.login'))
+    return wrap
+
+
+#####################
+###### routes #######
+#####################
+
+@app.route('/', defaults={'page': 'index'})
+def index(page):
+    return redirect(url_for('tasks.tasks'))
+
+
+@app.errorhandler(404)
+def internal_error(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n404 error at {}: {}".format(current_timestamp, r))
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n505 error at {}: {}".format(current_timestamp, r))
+    return render_template('500.html'), 500
+
+
+
+
+
+
